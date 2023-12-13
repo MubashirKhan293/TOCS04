4562b48c814842d7380971363fcecae8243fc531
MubashirKhan293 <146808869+MubashirKhan293@users.noreply.github.com>
2023-12-13 10:32:06 +0500

    Update Mubashir.py

--- Mubashir.py
+++ Mubashir.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def test():
-    return ('This is Assignment 04 of TOCS hello..')
+    return ('Assignment 04 of TOCS hello..')
 
 if __name__ == '__main__':
     app.run()
